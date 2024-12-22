d0f143b95a4a762cf6cddb3847353ad95e69ee94
VishalDahiya <visudahiya10025@gmail.com>
2024-12-22 21:28:30 +0530

    updating home page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar"
+import radicraftLogo from   "../public/assets/radicrafts.svg";
+import "../public/styles.css";
 
 
 function App() {
 
     return <>
-        <Navbar></Navbar></>
+        <Navbar></Navbar>
+        <div class="mainContent">
+            <div class="logo">
+                <img src={radicraftLogo} alt="radicraft logo"  />
+            </div>
+            <div class="tagline">
+                <p>We are building an amazing website where you can purchage, customize and consult gift items for your loved ones as well as home decor items</p>
+            </div>
+        </div>
+        </>
+        
 }
 
 export default App;
